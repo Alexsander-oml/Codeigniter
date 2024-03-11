630fff843dc5825ff8c0a5ffa33f47c320e73eae
Alexsander-oml <alexsander2018.acs@gmail.com>
2024-03-11 19:43:46 -0300

    CSS Geral

--- app/Views/resultados.php
+++ app/Views/resultados.php
@@ -10,37 +10,31 @@
     body {
       font-family: 'Arial', sans-serif;
       background: linear-gradient(to bottom left, #8A2BE2, #4B0082);
-      /* Gradiente de roxo para azul escuro */
       color: #FFF;
-      /* Texto branco */
       margin: 0;
       display: flex;
       align-items: center;
       justify-content: center;
       height: 100vh;
-      /* 100% da altura da viewport */
     }
 
     #result-box {
       background: #191970;
-      /* Azul escuro */
       padding: 20px;
       border-radius: 10px;
       text-align: center;
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-      /* Sombra leve */
+      
     }
 
     h2 {
       color: #00BFFF;
-      /* Azul claro */
     }
 
     p {
       margin: 10px 0;
     }
 
-    /* Adicione mais estilos conforme necessário */
   </style>
 </head>
 
