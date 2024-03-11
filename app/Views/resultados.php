e902128a79dd28351ba6b4c3013e9a8748410a99
Alexsander-oml <alexsander2016.acs@gmail.com>
2024-03-11 19:34:56 -0300

    CSS Geral

--- app/Views/resultados.php
+++ app/Views/resultados.php
@@ -5,14 +5,53 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Resultados</title>
+
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      background: linear-gradient(to bottom left, #8A2BE2, #4B0082);
+      /* Gradiente de roxo para azul escuro */
+      color: #FFF;
+      /* Texto branco */
+      margin: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      /* 100% da altura da viewport */
+    }
+
+    #result-box {
+      background: #191970;
+      /* Azul escuro */
+      padding: 20px;
+      border-radius: 10px;
+      text-align: center;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      /* Sombra leve */
+    }
+
+    h2 {
+      color: #00BFFF;
+      /* Azul claro */
+    }
+
+    p {
+      margin: 10px 0;
+    }
+
+    /* Adicione mais estilos conforme necessário */
+  </style>
 </head>
 
 <body>
-  <h2>Resultado</h2>
-  <p>Nome: <?= $nome; ?></p>
-  <p>Ano de nascimento: <?= $ano_nascimento; ?></p>
-  <p>Salário: R$<?= $salario; ?></p>
-  <p>Ressarcimento: R$<?= $ressarcimento; ?></p>
+  <div id="result-box">
+    <h2>Ressarcimento</h2>
+    <p>Nome: <?= $nome; ?></p>
+    <p>Ano de nascimento: <?= $ano_nascimento; ?></p>
+    <p>Salário: R$<?= $salario; ?></p>
+    <p>Ressarcimento: R$<?= $ressarcimento; ?></p>
+  </div>
 </body>
 
 </html>
